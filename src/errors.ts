5229e26200d5a63c5c7fcf9104472f6db01f52e6
Axel Hellström <axel.hellstrom@insurello.se>
2020-05-13 16:23:37 +0200

    wip

--- src/errors.ts
+++ src/errors.ts
@@ -15,12 +15,14 @@ const isError = (err: any): err is ErrorDescription =>
   (err as ErrorDescription).error_description !== undefined &&
   typeof (err as ErrorDescription).error_description === "string";
 
+const defaultDelayMs = 30000;
+
 const delay = (sleepMs: number): Promise<void> =>
   new Promise((resolve) => setTimeout(resolve, sleepMs));
 
 export const errorHandler = (req: Request, logger: Logger) => (
   err?: unknown
-) => {
+): Promise<void> => {
   const requestInfo = {
     type: req.type,
     queue: req.queue,
@@ -28,16 +30,21 @@ export const errorHandler = (req: Request, logger: Logger) => (
   };
 
   if (err instanceof Error) {
-    req.nack();
+    const delayMs =
+      typeof err.nackDelayMs === "number" ? err.nackDelayMs : defaultDelayMs;
     logger.error(err.stack ? err.stack : err.message, requestInfo);
+    return delay(delayMs).then(() => req.nack());
   } else if (isError(err)) {
     response(req)(err, { "x-error": err.error });
     logger.warn(JSON.stringify(err), requestInfo);
+    return Promise.resolve();
   } else if (typeof err === "string") {
     response(req)({ error: err }, { "x-error": err });
     logger.warn(JSON.stringify(err), requestInfo);
+    return Promise.resolve();
   } else {
     req.reject();
     logger.verbose("rejected", requestInfo);
+    return Promise.resolve();
   }
 };
